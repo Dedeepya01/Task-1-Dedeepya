9eb2e6066693e1cd5321e555e08e131370adc509
Dedeepya <kokkondadedeepya@gmail.com>
2026-05-29 19:24:46 +0530

    Number game

--- Numbergame.java
+++ Numbergame.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Numbergame {
+public class Numbergame
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
